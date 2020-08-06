ae8b525b4003c0d708927302cc1a40ac36bc969e
sanbo <sanbo.xyz@gmail.com>
2020-08-06 17:54:36 +0800

    update

--- app/src/main/java/cn/ref/MainActivity.java
+++ app/src/main/java/cn/ref/MainActivity.java
@@ -26,15 +26,7 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.btnA:
                 Logs.i("============即将展示不回调普通调用==========");
-                new RefCus().sayHello(" 手动调用接口 ", new ICallback() {
-                    
-                    @Override
-                    public void sayOver(String text) {
-                        System.out.println();
-                        Logs.i("接收到回调: [" + text + "]");
-                    }
-                    
-                });
+                generalCallBack();
                 break;
             case R.id.btnB:
                 Logs.i("============即将展示反射调用================");
@@ -45,13 +37,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     
+    /**
+     * 常规回调函数
+     */
+    private void generalCallBack() {
+        new RefCus().sayHello(" 手动调用接口 ", new ICallback() {
+            
+            //该方法反射时，处理写在Mhandler中
+            @Override
+            public void sayOver(String text) {
+                System.out.println();
+                Logs.i("接收到回调: [" + text + "]");
+            }
+            
+        });
+    }
     
+    
+    /**
+     * 反射实现下回调函数
+     */
     private void ref() {
         try {
             String text = "反射调用一下...";
             Class<?> refCus = Class.forName("cn.ref.model.RefCus");
             Class<?> callback = Class.forName("cn.ref.model.ICallback");
             final Mhandler mh = new Mhandler();
+            /**
+             * newProxyInstance( ClassLoader loader, Class<?>[] interfaces,  InvocationHandler h) ：
+             * ClassLoader loader：它是类加载器类型,Class对象就可以获取到ClassLoader对象；
+             * Class[] interfaces：指定newProxyInstance()方法返回的对象要实现哪些接口，可以指定多个接口;
+             * InvocationHandler h： 回调函数的处理器，如返回值为空，则处理器中返回null即可
+             *
+             */
             Object mObj = Proxy.newProxyInstance(getClassLoader(), new Class[]{callback}, mh);
             Method sayHello = refCus.getDeclaredMethod("sayHello", new Class[]{String.class, callback});
             sayHello.invoke(refCus.newInstance(), new Object[]{text, mObj});
--- app/src/main/java/cn/ref/rl/Mhandler.java
+++ app/src/main/java/cn/ref/rl/Mhandler.java
@@ -5,12 +5,21 @@ import java.lang.reflect.Method;
 
 import cn.ref.Logs;
 
+/**
+ * @Copyright © 2020 sanbo Inc. All rights reserved.
+ * @Description: 回调函数处理器
+ * @Version: 1.0
+ * @Create: 2020/8/6 17:38
+ * @author: sanbo
+ */
 public class Mhandler implements InvocationHandler {
     @Override
     public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
 //        Logs.i(method.toString());
         if ("sayOver".equals(method.getName())) {
+            // 此处处理的方法是<code> public void sayOver(String text);</code>
             Logs.i("回调方法： " + method.getName() + ", Mhandler 接收到回调: [" + objects[0].toString() + "]");
+            // 返回值是void,直接返回即可，如还反射调用，则会递归至爆栈
             return null;
         }
         return method.invoke(o, objects);
